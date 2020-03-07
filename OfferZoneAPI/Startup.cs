9141494b36db18a6280df5af5d6b8d6fd4abcd0f
dwipayant <dwi.tripathy@gmail.com>
2020-03-07 20:42:20 +0530

    API Tested with Postman and authorization

--- OfferZoneAPI/Startup.cs
+++ OfferZoneAPI/Startup.cs
@@ -52,7 +52,6 @@ namespace OfferZoneAPI
             services.AddTransient<AuthService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
